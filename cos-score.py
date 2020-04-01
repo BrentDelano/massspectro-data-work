fdea29f8baff091e3ab6b7c0651bde87b14a4dc1
BrentDelano <38890669+BrentDelano@users.noreply.github.com>
2020-04-01 16:18:19 -0700

    Sent data from files to arrays

--- cos-score.py
+++ cos-score.py
@@ -1,7 +1,16 @@
 import pyteomics
 import numpy as np
-from pyteomics import mgf, auxiliary
+from pyteomics import mgf
 
-with mgf.MGF('HMDB.mgf') as reader:
-    for spectrum in reader:
-    	print(spectrum['params'])
+mzs = []
+intensities = []
+
+def main():
+	with mgf.MGF('HMDB.mgf') as reader:
+	    for i, spectrum in enumerate(reader, start=1):
+	    	mzs.append(spectrum['m/z array'])
+	    	intensities.append(spectrum['intensity array'])	
+	    print(intensities)
+
+if __name__ == "__main__":
+    main()
