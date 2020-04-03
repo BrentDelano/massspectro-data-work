f1e4e1cdb7fadb7a177162579700a02102777fb9
BrentDelano <38890669+BrentDelano@users.noreply.github.com>
2020-04-03 16:00:02 -0700

    mass data and import GNPS workflow from github

--- cos-score.py
+++ cos-score.py
@@ -1,15 +1,21 @@
 import pyteomics
 import numpy as np
 from pyteomics import mgf
+import sys
+sys.path.insert(1, '/Users/brent/Desktop/cosScore/GNPS_Workflows')
+sys.path.insert(1, '/Users/brent/Desktop/cosScore/GNPS_Workflows/shared_code')
+import spectrum_alignment
 
-# 3D list of the spectra, holding the m/z ratios and intensities
+# Lists to hold the spectra (m/z ratios and intensities) and masses
 spectra = []
+masses = []
 
 def main():
 	# sort through HMDB file and place data into spectra list
 	with mgf.MGF('HMDB.mgf') as reader:
-	    for spectrum in reader:
-	   		spectra.append([spectrum['intensity array'].tolist(), spectrum['m/z array'].tolist()])
+		for spectrum in reader:
+			masses.append(spectrum['params']['pepmass'][0])
+			spectra.append([spectrum['intensity array'].tolist(), spectrum['m/z array'].tolist()])
 
 	# compare all spectra to each other; place cosine scores into an nxn array
 	cosScores = []
