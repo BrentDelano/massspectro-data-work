f7e6794f2ad02175f46e2b06213a2026d1cfb73c
BrentDelano <38890669+BrentDelano@users.noreply.github.com>
2020-04-03 19:33:11 -0700

    calculated cosine scores and outputted them into a .txt file

--- cos-score.py
+++ cos-score.py
@@ -9,23 +9,52 @@ import spectrum_alignment
 spectra = []
 masses = []
 
+# compare all spectra to each other; place cosine scores into an nxn array
+cosScores = []
+
+# Text file that cosine score data will be printed to
+cosScoreTxt = open('cos-score-data', 'w')
+
 def main():
 	# sort through HMDB file and place data into spectra list
 	with mgf.MGF('HMDB.mgf') as reader:
 		for spectrum in reader:
 			masses.append(spectrum['params']['pepmass'][0])
-			spectra.append([spectrum['intensity array'].tolist(), spectrum['m/z array'].tolist()])
+			temp = []
+			for i in range(len(spectrum['m/z array'])):
+				temp.append([spectrum['m/z array'][i], spectrum['intensity array'][i]])
+			spectra.append(temp)
 
-	# compare all spectra to each other; place cosine scores into an nxn array
-	cosScores = []
-	for i in spectra:
-		temp = []
-		for j in spectra:
+	# calculate first nxn cosine scores
+	# cosScoreTxt.write('[')
+	# for i in range(10):
+	# 	cosScoreTxt.write('[')
+	# 	for j in range(10):
+	# 		if (j > 0 and j < 10):
+	# 			cosScoreTxt.write(', ')
+	# 		if (i == j):
+	# 			cosScoreTxt.write('1.0')
+	# 		else:
+	# 			x = spectrum_alignment.score_alignment(spectra[i], spectra[j], masses[i], masses[j], 0.02)[0]
+	# 			cosScoreTxt.write(str(x))
+	# 	cosScoreTxt.write(']')
+	# cosScoreTxt.write(']')
+
+	# calculate all cosine scores
+	cosScoreTxt.write('[')
+	for i,rSpec in enumerate(spectra):
+		cosScoreTxt.write('[')
+		for j,cSpec in enumerate(spectra):
+			if (j > 0 and j < len(spectra)):
+				cosScoreTxt.write(', ')
 			if (i == j):
-				temp.append(1)
+				cosScoreTxt.write('1.0')
 			else:
-				temp.append(0)
-		cosScores.append(temp)
+				x = spectrum_alignment.score_alignment(rSpec, cSpec, masses[i], masses[j], 0.02)[0]
+				cosScoreTxt.write(str(x))
+		cosScoreTxt.write(']')
+	cosScoreTxt.write(']')
+
 
 if __name__ == "__main__":
-    main()
+	main()
