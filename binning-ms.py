9779c681dc93bde94d82f0323296db4797795fdb
BrentDelano <38890669+BrentDelano@users.noreply.github.com>
2020-04-06 14:44:22 -0700

    added a graphing feature that plots a histogram of the spectra as well as the probability density function

--- binning-ms.py
+++ binning-ms.py
@@ -5,20 +5,23 @@
 # The columns of the matrix are the spectra
 # If an m/z ratio of a spectra (m) fits into a bin (n), then the value (n,m) on the matrix will be the intensity value of that m/z ratio;
 #	if not, then it will be 0
-# Adds Gaussian noise to the dataset in order to visualize the effect on bins
+# Able to add Gaussian noise to the dataset in order to visualize the effect on bins
+# Plots histogram of m/z spectra in bins with relative frequency
 # Uses https://www.python-course.eu/pandas_python_binning.php for binning functions
+# Uses https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.random.normal.html for plots
 
 import pyteomics
 from pyteomics import mgf
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
 	# reads mgf file and initializes lists of m/z ratios and respective intensities
 	mzs = read_mgf('HMBD.mgf')[0]
 	intensities = read_mgf('HMBD.mgf')[1] 
 
-	# adds gaussian noise to the m/z dataset
+	# adds gaussian noise to the m/z dataset (comment this line if you don't want noise)
 	mzs = create_gaussian_noise(mzs)
 
 	# creates bins
@@ -37,6 +40,9 @@ def main():
 
 	# prints peaks matrix
 	print(create_peak_matrix(mzs, intensities, bins))
+
+	# graphs histogram
+	graph(mzs, len(bins), [minmz,maxmz])
 
 
 # takes in a .mgf file and reads it
@@ -87,14 +93,16 @@ def create_peak_matrix(mzs, intensities, bins):
 	return(peaks)
 
 
-# # adds gaussian noise to the m/z dataset
+# adds gaussian noise to the m/z dataset
 def create_gaussian_noise(mzs):
 	mzs_od = []
 	for mz in mzs:
 		for m in mz:
 			mzs_od.append(m)
 
-	mu,sigma = 0, 0.1
+	mu = np.mean(mzs_od)
+	sigma = np.std(mzs_od)
+
 	noise = np.random.normal(mu, sigma, len(mzs_od))
 	noisy = mzs_od + noise
 
@@ -112,6 +120,24 @@ def create_gaussian_noise(mzs):
 		i += 1
 
 	return(noisy_shaped)
+
+# Uses matplot lib and https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.random.normal.html to graph
+# 	m/z data on a histogram; 
+# Also plots the probability density function of the distribution (in red)
+def graph(mzs, numBins, r):
+	mzs_od = []
+	for mz in mzs:
+		for m in mz:
+			mzs_od.append(m)
+
+	mu = np.mean(mzs_od)
+	sigma = np.std(mzs_od)
+
+	count, bins, ignored = plt.hist(x=mzs_od, bins=numBins, density=True, range=r, histtype = 'bar', facecolor='blue')
+	plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
+	plt.ylabel('Frequency')
+	plt.xlabel('M/Z Bins')
+	plt.show()
 
 
 if __name__ == "__main__":
