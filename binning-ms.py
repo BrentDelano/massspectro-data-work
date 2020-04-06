24d79db567b43ba61b7d257efa07fa76e6d78fe5
BrentDelano <38890669+BrentDelano@users.noreply.github.com>
2020-04-06 00:43:50 -0700

    created a gaussian noise generator for the m/z array

--- binning-ms.py
+++ binning-ms.py
@@ -1,20 +1,25 @@
 # Written by Brent Delano
-# 4/4/2020
+# 4/6/2020
 # Takes a .mgf file and creates a matrix of where m/z ratios from spectra fit into bins of different sizes
 # The rows of the matrix are the bins (bin min is the lowest m/z value, truncated, and bin max at the greatest m/z value, rounded up) 
 # The columns of the matrix are the spectra
 # If an m/z ratio of a spectra (m) fits into a bin (n), then the value (n,m) on the matrix will be the intensity value of that m/z ratio;
 #	if not, then it will be 0
+# Adds Gaussian noise to the dataset in order to visualize the effect on bins
 # Uses https://www.python-course.eu/pandas_python_binning.php for binning functions
 
 import pyteomics
 from pyteomics import mgf
 import math
+import numpy as np
 
 def main():
 	# reads mgf file and initializes lists of m/z ratios and respective intensities
 	mzs = read_mgf('HMBD.mgf')[0]
 	intensities = read_mgf('HMBD.mgf')[1] 
+
+	# adds gaussian noise to the m/z dataset
+	mzs = create_gaussian_noise(mzs)
 
 	# creates bins
 	minmz = 0
@@ -30,7 +35,7 @@ def main():
 				maxmz = m
 	bins = create_bins(lower_bound=math.floor(minmz), width=1, quantity=math.ceil(maxmz-minmz))
 
-	# print peaks matrix
+	# prints peaks matrix
 	print(create_peak_matrix(mzs, intensities, bins))
 
 
@@ -80,6 +85,33 @@ def create_peak_matrix(mzs, intensities, bins):
 					temp[index] = [temp[index], intensities[i][j]]
 		peaks.append(temp)
 	return(peaks)
+
+
+# # adds gaussian noise to the m/z dataset
+def create_gaussian_noise(mzs):
+	mzs_od = []
+	for mz in mzs:
+		for m in mz:
+			mzs_od.append(m)
+
+	mu,sigma = 0, 0.1
+	noise = np.random.normal(mu, sigma, len(mzs_od))
+	noisy = mzs_od + noise
+
+	index = 0
+	noisy_shaped = []
+	i = 0
+	while i < len(mzs):
+		temp = []
+		j = 0
+		while j < len(mzs[i]):
+			temp.append(noisy[index])
+			index += 1
+			j += 1
+		noisy_shaped.append(temp)
+		i += 1
+
+	return(noisy_shaped)
 
 
 if __name__ == "__main__":
