cc44231ed29b7e5c5e9dda7456c89a670b6a4807
BrentDelano <38890669+BrentDelano@users.noreply.github.com>
2020-04-09 01:09:38 -0700

    deleted previous poorly named files and made the spectra_c

--- spectra_comparison.py
+++ spectra_comparison.py
@@ -10,8 +10,21 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.insert(1, './GNPS_Workflows/shared_code')
 import spectrum_alignment
-import cos-score
-import binning-ms
+import cos_score
+import binning_ms
 
+# reads in mgf files that have been passed in as a list of strings of file paths
 def read_mgfs(mgfs):
-	cos-score
+	cosine_mgf = cos_score.read_mgf_cosine(mgfs)
+	binning_mgf = binning_ms.read_mgf_binning(mgfs)
+	spectra, masses, mzs, intensities = cosine_mgf[0], cosine_mgf[1], binning_mgf[0], binning_mgf[1]
+	return spectra, masses, mzs, intensities
+
+
+# for testing
+def main():
+	mgf_data = read_mgfs(['./data/HMDB.mgf', './data/agp500.mgf', './data/agp3k.mgf'])
+	print(mgf_data[3])
+
+if __name__ == "__main__":
+	main()
