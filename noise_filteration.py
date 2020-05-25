4d5013dc145097652c8f75efeddaa53b26aebdf6
BrentDelano <38890669+BrentDelano@users.noreply.github.com>
2020-05-24 21:57:09 -0700

    created noise reduction function that creates large bins and chooses largest peaks in each bin

--- noise_filteration.py
+++ noise_filteration.py
@@ -6,8 +6,6 @@ import pyteomics
 from pyteomics import mgf, mzxml
 import math
 import numpy as np
-import matplotlib.pyplot as plt
-import copy
 from sklearn.decomposition import PCA
 import pickle
 import binning_ms
@@ -34,11 +32,100 @@ def read_mzxml(mzxml):
 	return mzs, intensities, identifiers
 
 
-# uses the binning_ms.py create_peak_matrix function (more documentation found there)
-# uses binWidth instead of bins (uses binning_ms.create_bins() to create bins of binWidth size)
-# THIS FUNCTION ALLOWS FOR THRESHOLD NOISE FILTERING THROUGH minIntens and maxIntens (default is minIntens=10, maxIntens=0)
-# If maxIntens = 0, then there is no maximum threshold
-def create_peak_matrix(mzs, intensities, identifiers, binWidth, listIfMultMZ=False, minIntens=10, maxIntens=0):
-	bins = binning_ms.create_bins(binWidth)
-	peaks = binning_ms.create_peak_matrix(mzs, intensities, identifiers, bins, minIntens)
-	return peaks
+# removes any peaks with an intensity less than min_intens
+# returns a count of the number of peaks removed
+def set_min_intens(mzs, intensities, min_intens):
+	poppers = []
+	count = 0
+	for i,spec in enumerate(intensities):
+		for j,intens in enumerate(spec):
+			if intens < min_intens:
+				poppers.append([i,j])
+				count += 1
+	for p in reversed(poppers):
+		mzs[p[0]].pop(p[1])
+		intensities[p[0]].pop(p[1])
+	return count
+
+
+# creates bins of width binsize of the m/z data for each spectra; sorts through peaks of each spectra and only keeps the k(peaks_per_bin) largest intensities of each bin
+# returns: [0]: # of removed spectra, [1]: # of affected spectra
+def choose_top_intensities(mzs, intensities, binsize, peaks_per_bin):
+	poppers = []
+	bins = binning_ms.create_bins(mzs, binsize)
+	for spec_num,spec in enumerate(mzs):
+		bs = []
+		for m in spec:
+			bs.append(binning_ms.find_bin(m, bins))
+		done = []
+		for j,b in enumerate(bs):
+			if b not in done or j == 0:
+				done.append(b)
+				temp = [j]
+				for i,s in enumerate(bs):
+					if i > j and b == s:
+						temp.append(i)
+				intens_temp = []
+				for t in temp:
+					intens_temp.append(intensities[spec_num][t])
+				int_inds = []
+				if peaks_per_bin < len(intens_temp):
+					rem = len(intens_temp) - peaks_per_bin
+					int_inds = np.argsort(intens_temp)[:rem] # indices of the peaks_per_bin smallest elements
+				indcs = []
+				for ind in int_inds:
+					indcs.append(temp[ind])
+				indcs.sort()
+				for ind in indcs:
+					poppers.append([spec_num, ind])
+
+	removed = len(poppers)
+	affected = []
+	for c,p in enumerate(reversed(poppers)):
+		if c == 0:
+			affected.append(p[0])
+		else:
+			if p[0] not in affected:
+				affected.append(p[0])
+		mzs[p[0]].pop(p[1])
+		intensities[p[0]].pop(p[1])
+	return removed, len(affected)
+
+
+# adds gaussian noise to the m/z dataset
+def create_gaussian_noise(mzs):
+	mzs_od = []
+	for mz in mzs:
+		for m in mz:
+			mzs_od.append(m)
+
+	mu = np.mean(mzs_od)
+	sigma = np.std(mzs_od)
+
+	noise = np.random.normal(mu, sigma, len(mzs_od))
+	noisy = mzs_od + noise
+
+	index = 0
+	noisy_shaped = []
+	i = 0
+	while i < len(mzs):
+		temp = []
+		j = 0
+		while j < len(mzs[i]):
+			temp.append(noisy[index])
+			index += 1
+			j += 1
+		noisy_shaped.append(temp)
+		i += 1
+
+	return noisy_shaped
+
+
+# for testing
+def main():
+	mgf_stuff = read_mgfs('./data/HMDB.mgf')
+	mzs, intensities = mgf_stuff[0], mgf_stuff[1]
+	choose_top_intensities(mzs, intensities, 100, 5)
+
+if __name__ == "__main__":
+	main()
