93151712d5b619b8e695f25f522e7cd9fba9c325
Asker Brejnrod <askerbrejnrod@gmail.com>
2021-01-18 01:42:25 -0800

    change dimensions, add options for filtering zero rows/columns, return csr matrix

--- fast_binner.py
+++ fast_binner.py
@@ -3,25 +3,48 @@ import numpy as np
 from scipy.sparse import dok_matrix
 import math
 import time
+import pickle as pkl
 
-start = time.time()
+
 mgf_file = "data/nematode_symbionts.mgf"
-min_bin = 50
-max_bin = 2000
-bin_size = 0.01
-bins = np.arange(min_bin, max_bin, bin_size)
 
-reader = mgf.IndexedMGF(mgf_file)
-X = dok_matrix((len(bins), len(reader.index)), dtype=np.float32)
+def filter_zero_cols(csr):
+    M = csr[:,csr.getnnz(0)>0]
+    return(csr)
+
+def filter_zero_rows(csr):
+    csr = csr[csr.getnnz(1)>0]
+    return(csr)
+
+def bin_sparse_dok(mgf_file, min_bin = 50, max_bin = 2000, bin_size = 0.01, verbose = False, remove_zero_sum_rows = True, remove_zero_sum_cols = True):
+    start = time.time()
+    bins = np.arange(min_bin, max_bin, bin_size)
 
 
-for spectrum_index, spectrum in enumerate(reader):
-    if len(spectrum['m/z array']) == 0:
-        continue
-#    if spectrum_index > 5:
-#        break
-    for mz, intensity in zip(spectrum['m/z array'], spectrum['intensity array']):
-        target_bin = math.floor((mz - min_bin)/bin_size)
-        X[target_bin, spectrum_index] += intensity
-print("Binned in %s seconds" % (time.time()-start))
-    
+    reader = mgf.IndexedMGF(mgf_file)
+    X = dok_matrix((len(reader.index), len(bins)), dtype=np.float32)
+
+    scan_names = []
+    for spectrum_index, spectrum in enumerate(reader):
+        if len(spectrum['m/z array']) == 0:
+            continue
+        for mz, intensity in zip(spectrum['m/z array'], spectrum['intensity array']):
+            target_bin = math.floor((mz - min_bin)/bin_size)
+            X[ spectrum_index,target_bin] += intensity
+            scan_names.append(spectrum['params']['scans'])
+
+    X = X.tocsr()
+    X_orig_shape = X.shape
+    if remove_zero_sum_rows:
+        X = filter_zero_rows(X)
+
+    if remove_zero_sum_cols:
+        X = filter_zero_cols(X)
+        
+    if verbose:
+            print("Binned in %s seconds with dimensions %sx%s, %s nonzero entries (%s), removed %s rows" % (time.time()-start, len(reader.index), len(bins), X.count_nonzero(), X.count_nonzero()/(len(reader.index)*len(bins)), X_orig_shape[0] - X.shape[0]))
+
+    return(X, bins, scan_names)
+
+
+X, bins, scan_names = bin_sparse_dok(mgf_file, verbose = True)
