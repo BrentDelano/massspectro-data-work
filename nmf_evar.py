10da61bca7ac7611f2d89c069a4fd196818b895a
Arjun Sampath <arjundsampath@gmail.com>
2021-02-05 12:52:18 -0800

    added pickle serialization

--- nmf_evar.py
+++ nmf_evar.py
@@ -4,6 +4,7 @@ import nimfa
 import time
 import fast_binner
 import pandas as pd
+import pickle as pkl
 
 mgf_data = [sys.path[0] + "/data/agp3k.mgf", sys.path[0] + "/data/MSV000082354.mgf", sys.path[0] + "/data/MSV000082374.mgf", sys.path[0] + "/data/MSV000083462.mgf", sys.path[0] + "/data/MSV000083759.mgf"]
 bin_size = 0.1
@@ -14,8 +15,15 @@ input_data, bins, scan_names = fast_binner.bin_sparse_dok(mgf_files = mgf_data, 
 
 start = time.time()
 nmf_model = nimfa.Nmf(input_data)
-evar = nmf_model().fit.evar()
+model = nmf_model()
+evar = model.fit.evar()
 end = time.time()
 
 print('Evar: {0}'.format(evar))
 print('It took {0:0.1f} seconds to fit NMF model and calculate EVar'.format(end - start))
+
+start = time.time()
+output_file = "model.pkl"
+pkl.dump((model.fit),open(output_file, "wb"))
+end = time.time()
+print('It took {0:0.1f} seconds to serialize model fit object'.format(end - start))
